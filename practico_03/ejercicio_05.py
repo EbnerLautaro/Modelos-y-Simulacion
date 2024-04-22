93734249e3dc416a0051c2fe8aba6d116cde2f13
EbnerLautaro <lautaroebner1@gmail.com>
2024-04-22 11:48:11 -0300

    ejercicio 5.f

--- practico_03/ejercicio_05.py
+++ practico_03/ejercicio_05.py
@@ -62,16 +62,13 @@ class MonteCarlo:
 
         return integral / n_sim
 
-
     def monte_carlo_n_vars_a_b(self, g, a: list, b: list, n_vars, n_sim):
         """
-        Aproximacion de la integral de la funcion g (de n variables) sobre los intervalos dados por a_i, b_i.
+        Aproximacion de la integral de la funcion g (de n variables) sobre los intervalos dados por a, b.
         Aclaracion:
             si la funcion es g(x, y) entonces los parametros a y b 
             tiene que ser a=[x_a, y_a], b=[x_b, y_b] en ese orden.
         """
-        
-        
         integral = 0
 
         for _ in range(n_sim):
@@ -126,51 +123,54 @@ if __name__ == "__main__":
         return math.e ** ((x+y) ** 2)
 
     def func_f(x, y):
-        return math.e ** -(x+y)
+
+        def I(a,b):
+            if a < b:
+                return 1
+            return 0
+
+        return math.e ** -(x+y) * I(x,y)
 
 
     mc = MonteCarlo()
 
 
-    # print("\n============ a ============")
-    # for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
-    #     aprox = mc.monte_carlo(func_a, n)
-    #     print(f"n: {n:<10} aproximacion: {aprox:<12}")
-    # print("Resultado exacto: 0.5890486225")
+    print("\n============ a ============")
+    for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
+        aprox = mc.monte_carlo(func_a, n)
+        print(f"n: {n:<10} aproximacion: {aprox:<12}")
+    print("Resultado exacto: 0.5890486225")
 
-    # print("\n============ b ============")
-    # for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
-    #     aprox = mc.monte_carlo_a_b(g=func_b,a=2, b=3, n_sim=n)
-    #     print(f"n: {n:<10} aproximacion: {aprox:<10}")
-    # print("Resultado exacto: 0.49041")
-
-
-    # print("\n============ c ============")
-    # for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
-    #     aprox = mc.monte_carlo_0_inf(g=func_c, n_sim=n)
-    #     print(f"n: {n:<10} aproximacion: {aprox:<10}")
-    # print("Resultado exacto: 0.5")
-
-    # print("\n============ d ============")
-    # for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
-    #     aprox = mc.monte_carlo_0_inf(g=func_d, n_sim=n)
-    #     print(f"n: {n:<10} aproximacion: {aprox:<10}")
-    # print("Resultado exacto: 1.77245385090")
-
-    # print("\n============ e ============")
-    # for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
-    #     aprox = mc.monte_carlo_n_vars(g=func_e, n_vars=2, n_sim=n)
-    #     print(f"n: {n:<10} aproximacion: {aprox:<10}")
+    print("\n============ b ============")
+    for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
+        aprox = mc.monte_carlo_a_b(g=func_b,a=2, b=3, n_sim=n)
+        print(f"n: {n:<10} aproximacion: {aprox:<10}")
+    print("Resultado exacto: 0.49041")
 
 
+    print("\n============ c ============")
+    for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
+        aprox = mc.monte_carlo_0_inf(g=func_c, n_sim=n)
+        print(f"n: {n:<10} aproximacion: {aprox:<10}")
+    print("Resultado exacto: 0.5")
 
+    print("\n============ d ============")
+    for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
+        aprox = mc.monte_carlo_0_inf(g=func_d, n_sim=n)
+        print(f"n: {n:<10} aproximacion: {aprox:<10}")
+    print("Resultado exacto: 1.77245385090")
 
+    print("\n============ e ============")
+    for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
+        aprox = mc.monte_carlo_n_vars(g=func_e, n_vars=2, n_sim=n)
+        print(f"n: {n:<10} aproximacion: {aprox:<10}")
+    print("Resultado exacto: 4.89916")
 
-    def f_random(x,y):
-        return (math.e) ** ((x+y)**(2/5))
-
-
+    print("\n============ f ============")
+    for n in [100, 1_000, 10_000, 100_000, 1_000_000]:
+        aprox = mc.monte_carlo_n_vars_0_inf(g=func_f, n_vars=2, n_sim=n)
+        print(f"n: {n:<10} aproximacion: {aprox:<10}")
+    print("Resultado exacto: 0.5")
 
 
 
-    print(mc.monte_carlo_n_vars_a_b(g=f_random, a=[1,8.9], b=[2,9], n_vars=2, n_sim=1_000_000))
