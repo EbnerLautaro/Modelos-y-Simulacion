56c790b5b47714597b3c69abe10d287c7202cd49
EbnerLautaro <lautaroebner1@gmail.com>
2024-05-10 17:29:32 -0300

    ejercicio 8 :pencil:

--- practico_04/ejercicio_08.py
+++ practico_04/ejercicio_08.py
@@ -1,6 +1,9 @@
 import math
 import random
 
+LAMDA = 0.7
+K = 10
+
 
 def cociente(lamda, k):
     sumatoria = 0
@@ -29,16 +32,47 @@ def transformada_inversa(lamda, k):
     return i
 
 
-def aceptacion_rechazo():
-    pass
+def aceptacion_rechazo(px, py, sim_y, c):
+
+    while True:
+        y = sim_y()
+        u = random.random()
+        if u < (px(y)/(py(y)*c)):
+            return y
 
 
 if __name__ == "__main__":
-    n_sim = 10_000
-    k = 10
-    resultados = [0 for _ in range(k)]
+
+    def p_x(x):
+        aux = (math.exp(-LAMDA) * (LAMDA**x)) / math.factorial(x)
+        return aux / cociente(k=10, lamda=LAMDA)
+
+    def p_y(y):
+        return (math.exp(-LAMDA) * (LAMDA**y)) / math.factorial(y)
+
+    c = max(p_x(i)/p_y(i) for i in range(K))
+
+    def poisson():
+        u = random.random()
+        i = 0
+        p = math.exp(-LAMDA)
+        acumulada = p
+        while u >= acumulada:
+            i += 1
+            p *= LAMDA / i
+            acumulada += p
+
+        return i
+
+    n_sim = 1_000
+    cont_ti = 0
+    cont_ar = 0
     for _ in range(n_sim):
-        sim = transformada_inversa(k=k, lamda=0.7)
-        resultados[sim] += 1
-    print(resultados)
-    print([elem/n_sim for elem in resultados])
+        sim_ti = transformada_inversa(k=K, lamda=LAMDA)
+        sim_ar = aceptacion_rechazo(px=p_x, py=p_y, sim_y=poisson, c=c)
+        cont_ti += int(sim_ti > 2)
+        cont_ar += int(sim_ar > 2)
+
+    print("P(X>2):")
+    print(f"\ttransformada_inversa: {cont_ti/n_sim} ")
+    print(f"\taceptacion_rechazo:   {cont_ar/n_sim}")
